7fc4a151e93b25f1d4a42dd6d7f83809cdaec29f
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-04 20:24:42 -0500

    added html elements and js variables

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var trackInput = document.getElementById("searchInput");
+var searchButton = document.getElementById("searchButton");
+searchButton.onclick = searchDatabase;
+searchDatabase = function() {
+    var musicResults
+}
