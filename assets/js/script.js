857c97fbc69092bb3584d2b1bb99fbf7b7c6dc3d
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-06 12:38:03 -0500

    added top tracks for artists searched by user and returns that information on the console.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,30 @@ var searchButton = document.querySelector("#searchButton");
 getArtist = function () {
     var apiUrl = "https://api.spotify.com/v1/search?q=" + trackInput.value + "&type=artist";
     var headers = {
-        Authorization:"Bearer BQAgai_F3COLcngzJasiaA0MFrAJp0aWWKO-Y4b1YQ_k4D6v5_WkThdraWye6fgKp7l3YJM9JySLX1Xs16EZNrADoeMSVaciYzTJp5yheYGOytpygpJWW_8M3sfkmu9CYwM8Q5R9QQATo82b5HlSuxxxDOekukU",
-        Accept:"application/json"
+        Authorization: "Bearer BQAgai_F3COLcngzJasiaA0MFrAJp0aWWKO-Y4b1YQ_k4D6v5_WkThdraWye6fgKp7l3YJM9JySLX1Xs16EZNrADoeMSVaciYzTJp5yheYGOytpygpJWW_8M3sfkmu9CYwM8Q5R9QQATo82b5HlSuxxxDOekukU",
+        Accept: "application/json"
     }
-    fetch(apiUrl, {headers:headers}).then(function (response) {
+    fetch(apiUrl, { headers: headers }).then(function (response) {
         console.log(response);
-        return(response.json());
-    }).then(function(data) {
+        return (response.json());
+    }).then(function (data) {
         console.log(data);
+
+        var artistId = data.artists.items[0].id;
+        console.log(artistId);
+        var apiUrl = "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?market=US";
+        var headers = {
+            Authorization: "Bearer BQAgai_F3COLcngzJasiaA0MFrAJp0aWWKO-Y4b1YQ_k4D6v5_WkThdraWye6fgKp7l3YJM9JySLX1Xs16EZNrADoeMSVaciYzTJp5yheYGOytpygpJWW_8M3sfkmu9CYwM8Q5R9QQATo82b5HlSuxxxDOekukU",
+            Accept: "application/json"
+        }
+        fetch(apiUrl, { headers: headers }).then(function (response) {
+            console.log(response);
+            return (response.json());
+        }).then(function(data) {
+        console.log(data);
+        })
     })
 };
-       
+
 
 searchButton.addEventListener("click", getArtist);
