8cf937b24f243c7835fd5f1548ab787132a8e68c
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-06 16:51:43 -0500

    link now opens in new tab and previous user input is cleared on new submit click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,20 @@ var topTracksEl = document.querySelector("#searchResults");
 var artistLinkEl = document.querySelector("#artistLink");
 var accessToken = "Bearer BQBnc9XG_qpiCXi7H1qPEpzrJaS2EUZbMeIk__Ef8EXAJR5dWT6OULRhfAUG1dVJi8pAZadMA06BXXdWdjfTMmJ9M8aqG7QlkbzvtysSP8fKev0xgPOwdR6sBlcPCD1w9qa3q6frnxk9mkoLKRNx5PnDp4dnTQA"
 getArtist = function () {
+    topTracksEl.innerHTML = "";
+    artistLinkEl.innerHTML = "";
+
     var apiUrl = "https://api.spotify.com/v1/search?q=" + trackInput.value + "&type=artist";
     var headers = {
         Authorization: accessToken,
         Accept: "application/json"
     }
     fetch(apiUrl, { headers: headers }).then(function (response) {
-        console.log(response);
         return (response.json());
     }).then(function (data) {
-        console.log(data);
         var artistName = data.artists.items[0].name;
         var artistId = data.artists.items[0].id;
-        var artistLink = data.artists.items[0].external_urls.spotify; 
-        console.log(artistId);
-        console.log(artistName);
+        var artistLink = data.artists.items[0].external_urls.spotify;
         var apiUrl = "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?market=US";
         var headers = {
             Authorization: accessToken,
@@ -27,16 +26,16 @@ getArtist = function () {
         fetch(apiUrl, { headers: headers }).then(function (response) {
             console.log(response);
             return (response.json());
-        }).then(function(data) {
-        console.log(data);
-        console.log(data.tracks[0].name)
-        var anchor = document.createElement('a');
-        var linkText = document.createTextNode("Click here to visit " + artistName + "'s page!");
-        anchor.appendChild(linkText);
-        anchor.title = "Click here to visit " + artistName + "'s page!";
-        anchor.href = artistLink; 
-        topTracksEl.innerHTML = ["Top five songs for " + artistName + ":" + data.tracks[0].name, data.tracks[1].name, data.tracks[2].name, data.tracks[3].name, data.tracks[4].name];
-        artistLinkEl.appendChild(anchor);  
+        }).then(function (data) {
+            var anchor = document.createElement('a');
+            var linkText = document.createTextNode("Click here to visit " + artistName + "'s page!");
+            var tracksText = document.createTextNode("Top five songs for " + artistName + ":" + data.tracks[0].name + ", " + data.tracks[1].name + ", " + data.tracks[2].name + ", " + data.tracks[3].name + ", " + data.tracks[4].name);
+            anchor.appendChild(linkText);
+            anchor.title = "Click here to visit " + artistName + "'s page!";
+            anchor.href = artistLink
+            anchor.target = "_blank";
+            topTracksEl.appendChild(tracksText);
+            artistLinkEl.appendChild(anchor);
         })
     })
 };
