5bc52ba5717a51e356f349c3f58df0e13bba7f00
Benicio Lopez <benicio.lopez@gmail.com>
2021-08-06 12:10:43 -0500

    getArtist function works and returns data from user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,16 @@ var trackInput = document.querySelector("#searchInput");
 var searchButton = document.querySelector("#searchButton");
 
 getArtist = function () {
-    var apiUrl = "https://api.spotify.com/v1/artists/{0OdUWJ0sBjDrqHygGUXeCF}";
-    fetch(apiUrl).then(function (response) {
+    var apiUrl = "https://api.spotify.com/v1/search?q=" + trackInput.value + "&type=artist";
+    var headers = {
+        Authorization:"Bearer BQAgai_F3COLcngzJasiaA0MFrAJp0aWWKO-Y4b1YQ_k4D6v5_WkThdraWye6fgKp7l3YJM9JySLX1Xs16EZNrADoeMSVaciYzTJp5yheYGOytpygpJWW_8M3sfkmu9CYwM8Q5R9QQATo82b5HlSuxxxDOekukU",
+        Accept:"application/json"
+    }
+    fetch(apiUrl, {headers:headers}).then(function (response) {
         console.log(response);
+        return(response.json());
+    }).then(function(data) {
+        console.log(data);
     })
 };
        
